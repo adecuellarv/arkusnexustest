181f0e77e48854ce0b56278ee8d022149a1944a1
adecuellarv <ade.cuellar92@gmail.com>
2021-01-17 13:48:18 -0600

    estructura p1

---
+++ src/components/modal-edit/modal-edit.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ModalEdit() {
+  return (
+    <div className="App">
+      <h1>Inicio del proyecto</h1>
+    </div>
+  );
+}
+
+export default ModalEdit;
---
+++ src/components/user-list/user-list.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserList() {
+  return (
+    <div className="App">
+      <h1>Inicio del proyecto</h1>
+    </div>
+  );
+}
+
+export default UserList;
